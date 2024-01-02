063a5da23bf9ef0a7d3d5a484c9f5f59520a98b3
Jtownokie <6673@holbertonstudents.com>
2024-01-02 16:20:25 -0600

    Set up repo/directory for project

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+// Showcasing the use cases for const vs. let variable declaration
+
+export function taskFirst() {
+  var task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  var combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
