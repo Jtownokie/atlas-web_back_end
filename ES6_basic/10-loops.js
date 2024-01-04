cd26519265716775c99d9b436074c20ea5387526
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-04 00:40:20 -0600

    Completed for...of loop

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,1 +1,9 @@
-// Placeholder
+// for of loop
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let [index, item] of array.entries()) {
+    array[index] = appendString + item;
+  }
+
+  return array;
+}
