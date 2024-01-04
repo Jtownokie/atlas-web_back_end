bb908acabaa92e9099f1ed663eeda42f7e36f498
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-03 21:45:13 -0600

    Adjusted eslint err ignore comment

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,14 +1,13 @@
 // Modifying declarations to prevent overwrite
-/* exported task */
-/* exported task2 */
+/* eslint-disable no-unused-vars */
 
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
