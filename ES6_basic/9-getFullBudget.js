14133d473af9d2af60ed0a75c1ac13373db33a80
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-04 00:10:23 -0600

    Completed method property task

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,1 +1,18 @@
-// Placeholder
+// ES6 Method Properties
+
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
