3a54aa2a2b916c09388790754b6a6d3cc054df85
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-03 21:23:54 -0600

    Completed block scoping task

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,1 +1,13 @@
-// Placeholder
+// Modifying declarations to prevent overwrite
+
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
