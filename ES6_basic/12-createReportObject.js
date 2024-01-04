cce0bea1720f390336ca1b23c8876df6241cb7c2
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-04 01:25:55 -0600

    Completed report script task

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,1 +1,16 @@
-// Placeholder
+// Report creation script
+
+export default function createReportObject(employeesList) {
+  const newObject = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      const numDepartments = Object.keys(employeesList).length;
+
+      return numDepartments;
+    }
+  }
+
+  return newObject;
+}
