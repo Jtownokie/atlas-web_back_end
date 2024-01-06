d7fada45aedbbc9231880182e5ae8ad063711423
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-05 23:45:58 -0600

    Completed casting task

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+// Overwrite casting
+
+export default class Airport {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size
+  }
+}
