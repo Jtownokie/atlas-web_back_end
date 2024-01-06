2056834f2689a3b9fd1276b4ebce7a2ee73d3075
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-05 20:57:44 -0600

    Completed object instantation function task

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from "./0-classroom";
+
+export default function initializeRooms() {
+  const objOne = new ClassRoom(19);
+  const objTwo = new ClassRoom(20);
+  const objThree = new ClassRoom(34);
+
+  return [objOne, objTwo, objThree];
+}
