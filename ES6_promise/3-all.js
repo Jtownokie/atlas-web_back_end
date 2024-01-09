c92dac027d233b6c4cf709bdda93ee6d217b7849
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-09 17:21:38 -0600

    Corrected catch statement

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then(([photoResult, userResult]) => {
     console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
   })
-    .catch((error) => {
-      console.error('Signup system offline', error);
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
