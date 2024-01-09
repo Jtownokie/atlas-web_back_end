686325db75781ebec20b8b2017b74e835ab5549b
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-09 17:19:52 -0600

    Corrected return value

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,10 +2,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then(([photoResult, userResult]) => {
+  return Promise.all([uploadPhoto(), createUser()]).then(([photoResult, userResult]) => {
     console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
   })
-  .catch(error => {
-    console.error('Signup system offline', error);
-  });
+    .catch((error) => {
+      console.error('Signup system offline', error);
+    });
 }
