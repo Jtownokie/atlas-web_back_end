7bf8872fc3a021f6227a6ebe56670768a3a4a47c
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-09 17:27:56 -0600

    Corrected for ESLint

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -2,6 +2,6 @@
 
 export default function signUpUser(firstName, lastName) {
   return new Promise((resolve) => {
-    resolve({ firstName: firstName, lastName: lastName });
+    resolve({ firstName, lastName });
   });
 }
