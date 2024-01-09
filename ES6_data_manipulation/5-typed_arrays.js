de539a2c58e9b4cfbea2619f58c4a2fa7b17bc2a
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-09 10:41:25 -0600

    Added catch statement for setInt8 failure

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,12 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
-  view.setInt8(position, value);
+
+  try {
+    view.setInt8(position, value);
+  } catch {
+    throw 'Position outside range'
+  }
 
   return view;
 }
