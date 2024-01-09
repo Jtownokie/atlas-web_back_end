fe370ca1f6edd5cecb36ce229d0c95d0a21a563c
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-09 16:15:10 -0600

    Correcting for ESLint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 // Resolving/Rejecting Promises
 
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => { return { body: 'success', status: 200 } }, () => new Error()).finally(console.log('Got a response from the API'));
+  return promise.then(function() { return { body: 'success', status: 200 } }, function() { new Error() }).finally(console.log('Got a response from the API'));
 }
