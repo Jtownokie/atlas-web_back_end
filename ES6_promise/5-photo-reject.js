710cd55046a983501e099b92d65311820c46114b
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-09 17:33:16 -0600

    Completed simple Reject Function

---
+++ ES6_promise/5-photo-reject.js
@@ -0,0 +1,7 @@
+// Promise Rejection
+
+export default function uploadPhoto(fileName) {
+  return new Promise((resolve, reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
+  });
+}
