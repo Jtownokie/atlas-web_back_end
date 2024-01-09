4d4d5c81d928b993f5b2908163b6328eeb7fd12a
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-09 16:19:10 -0600

    Added ESLint ignore message

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,6 @@
 // Resolving/Rejecting Promises
+/* eslint-disable arrow-body-style */
 
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => { return { body: 'success', status: 200 } }, () => new Error()).finally(console.log('Got a response from the API'));
+  return promise.then(() => { return { body: 'success', status: 200 }; }, () => new Error()).finally(console.log('Got a response from the API'));
 }
