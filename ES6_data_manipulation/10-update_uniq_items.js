f32d64e3790fff410647dc15b489b1cbf3c9feee
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-09 14:47:37 -0600

    Completed Map Value Update Function Task

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+// Updating Map Values
+
+export default function updateUniqueItems(map) {
+  if (!map instanceof Map) {
+    throw new Error('Cannot process');
+  }
+
+  const updatedMap = map.forEach((value, key, map) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+
+  return updatedMap;
+}
