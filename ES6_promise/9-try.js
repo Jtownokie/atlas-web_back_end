34dc696e96a52a25598766eaf617ee184b035f48
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-10 09:00:51 -0600

    Corrected for ESLint

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,16 +1,16 @@
 // Using Try/Catch
 
 export default function guardrail(mathFunction) {
-  const queue = []
+  const queue = [];
 
   try {
-    const result = mathFunction()
+    const result = mathFunction();
     queue.push(result);
   } catch (error) {
     queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
-
-    return queue;
   }
+
+  return queue;
 }
