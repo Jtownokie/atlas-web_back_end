6829793237e504db97b49f4d44eeea667696070d
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-09 19:33:40 -0600

    Corrected array

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,6 +4,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(([signUpResult, uploadResult]) => {
-    return [{ status: signUpResult.status, value: signUpResult.value }, { status: uploadResult.status, value: uploadResult.value }];
+    return [
+      { status: 'fulfilled', value: { firstName: signUpResult.firstName, lastName: signUpResult.lastName } },
+      { status: 'rejected', value: `Error: ${uploadResult.message}` }
+    ];
   })
 }
