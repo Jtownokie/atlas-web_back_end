4feb6c559b9c58a05b2d4735e4ab16c466b0386b
Jtownokie <christopher.stephens@atlasschool.com>
2024-04-29 12:14:28 -0500

    Completed task 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""
+This module contains the floor() function practicing type hints
+"""
+
+import typing
+
+
+def floor(n: float) -> int:
+    """ This function takes a float and returns its floor """
+    return int(n)
