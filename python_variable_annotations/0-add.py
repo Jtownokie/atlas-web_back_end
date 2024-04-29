0194e810777d72c9cf32bca544ceb1c51b0b46fe
Jtownokie <christopher.stephens@atlasschool.com>
2024-04-29 11:29:36 -0500

    Created directory, completed task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains the add() function practicing type hints
+"""
+
+import typing
+
+
+def add(a: float, b: float) -> float:
+    """ This function takes two floats and returns their sum as a float """
+    return a + b
