e288deb5f630ee3f0bf1168ace2245f346135c79
Jtownokie <christopher.stephens@atlasschool.com>
2024-04-29 15:11:41 -0500

    Correcting for mypy

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,9 +4,9 @@ This module contains the to_kv() function practicing type hints
 """
 
 import typing
-from typing import List, Union
+from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ This function takes a str and int OR float and returns tuple """
     return (k, v * v)
