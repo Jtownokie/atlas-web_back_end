685f61a72a8c84d1af53080bce41e63b43496eff
Jtownokie <christopher.stephens@atlasschool.com>
2024-04-29 12:36:46 -0500

    Completed task 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""
+This module contains the to_str() function practicing type hints
+"""
+
+import typing
+
+
+def to_str(n: float) -> str:
+    """ This function takes a float and returns its floor """
+    return str(n)
