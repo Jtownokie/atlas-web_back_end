e08b15e36abfd0b03a19be414c6ecf3861b3ae19
Jtownokie <christopher.stephens@atlasschool.com>
2024-04-29 15:32:32 -0500

    Completed task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+This module contains the make_multiplier() function practicing type hints
+"""
+
+import typing
+from typing import List, Union, Tuple, Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ This function takes a float and returns a function """
+    def func(num: float) -> float:
+        return num * multiplier
+
+    return func
+
