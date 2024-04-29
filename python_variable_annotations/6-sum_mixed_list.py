44e2199620fbe982eed1bbd6bbe818e27be42b0b
Jtownokie <christopher.stephens@atlasschool.com>
2024-04-29 15:05:08 -0500

    Completed task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+This module contains the sum_mixed_list() function practicing type hints
+"""
+
+import typing
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """ This function takes a list of ints and floats and returns its sum """
+    return sum(mxd_list)
