0d11f36d005376b853a1e5ff4ee8234c4dd00979
Jtownokie <christopher.stephens@atlasschool.com>
2024-04-29 15:36:48 -0500

    Completed task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+This module contains the element_length() function practicing type hints
+"""
+
+import typing
+from typing import List, Union, Tuple, Callable, Sequence, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ This function takes any Sequence and returns a list of tuples """
+    return [(i, len(i)) for i in lst]
