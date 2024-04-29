1e5c91ebbeeb39447358ab965e125ae507a16cc6
Jtownokie <christopher.stephens@atlasschool.com>
2024-04-29 14:07:00 -0500

    Completed task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+This module contains the sum_list() function practicing type hints
+"""
+
+import typing
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ This function takes a list of floats and returns the sum of its values """
+    return sum(input_list)
