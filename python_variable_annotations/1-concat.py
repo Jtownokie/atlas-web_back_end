d0521863dc52e5c2a9ff3d17620ab76a79a5fc7c
Jtownokie <christopher.stephens@atlasschool.com>
2024-04-29 11:36:21 -0500

    Completed task 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""
+This module contains the concat() function practicing type hints
+"""
+
+import typing
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This function takes two strings and returns them concatenated """
+    return str1 + str2
