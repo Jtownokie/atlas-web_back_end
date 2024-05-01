6782834737f6fa2ad5e9b80e64e4d882e9938c68
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-01 14:21:11 -0500

    Added type annotations

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,9 +5,10 @@ This module contains wait_random() coroutine to practice async operations
 
 import asyncio
 import random
+import typing
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """ This coroutine waits for a random delay between 0 and max_delay """
     delay = random.uniform(0, max_delay)
 
