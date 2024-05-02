93f3d0afee42910db4c97fb41d7d3765ba951d1c
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-02 12:21:42 -0500

    Completed task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module contains measure_runtime() coroutine to practice async
+"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ This coroutine measures runtime on async comprehension """
+    start = time.perf_counter()
+
+    asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+
+    elapsed = time.perf_counter() - start
+
+    return elapsed
