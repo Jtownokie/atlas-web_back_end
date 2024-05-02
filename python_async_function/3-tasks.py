a6604e7e402a3157da008107bb452b0622746fb1
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-02 10:54:52 -0500

    Completed task 3

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module contains task_wait_random() function to practice async operations
+"""
+
+import asyncio
+import time
+from typing import Awaitable
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Awaitable:
+    """ This function returns a Task object """
+    new_task = asyncio.create_task(wait_random(max_delay))
+
+    return new_task
