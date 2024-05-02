663b0757145d7830fe8aaf98f45c2ea484557e87
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-02 12:13:54 -0500

    Corrected task 1 to use list comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,9 +10,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """ This coroutine uses async comprehension to collect 10 random nums """
-    result = []
-
-    async for i in async_generator():
-        result.append(i)
+    result = [i async for i in async_generator()]
 
     return result
