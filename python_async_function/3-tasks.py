a26f23e172d42191959b5d3eb539b2f43fc5a7f6
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-02 10:56:19 -0500

    Correcting for type hinting

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -5,11 +5,11 @@ This module contains task_wait_random() function to practice async operations
 
 import asyncio
 import time
-from typing import Awaitable
+import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ This function returns a Task object """
     new_task = asyncio.create_task(wait_random(max_delay))
 
