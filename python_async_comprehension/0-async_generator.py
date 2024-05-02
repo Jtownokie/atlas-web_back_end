c6f989cf25c071674f4b406ad71ca762b065012f
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-02 11:59:29 -0500

    Built directory, completed task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module contains async_generator() coroutine to practice async operations
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ This coroutine loops 10 times async, waiting 1 second each time """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
