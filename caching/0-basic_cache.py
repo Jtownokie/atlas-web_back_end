920741b6e155273cb6cfa5a9c94954c1a1e44fb6
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-06 10:52:19 -0500

    Built Directory, Completed task 0

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    BasicCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """ BasicCache defines:
+        - put(): Setter Type Method
+        - get(): Getter Type Method
+    """
+
+    def __init__(self):
+        """ 
+            Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
