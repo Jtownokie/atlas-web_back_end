c9df728dab2694ddd5261ba68375d9724019fa86
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-19 17:48:15 -0500

    Completed task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,3 +2,14 @@
 """
     Filtered Logger Module
 """
+# import logging
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """ Filter Datum Method """
+    for field in fields:
+        field_pattern = rf"({field}=)[^{separator}]*({separator})"
+        message = re.sub(field_pattern, rf"\1{redaction}\2", message)
+
+    return message
