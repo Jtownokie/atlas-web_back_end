7a6aabe4feb9788c11ee5be49a911e457375f9fa
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-21 22:19:23 -0500

    Completed task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -9,3 +9,11 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ Returns a hashed, salted password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validates hashed password """
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
