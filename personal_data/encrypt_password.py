b4a6a145e07de78aca81120123bd4626faccc76d
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-21 22:15:19 -0500

    Completed task 5

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -2,3 +2,10 @@
 """
     Password Encryption Module
 """
+import typing
+import bcrypt
+
+
+def hash_password(password: str) -> typing.ByteString:
+    """ Returns a hashed, salted password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
