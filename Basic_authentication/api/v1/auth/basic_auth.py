d956b0bc30178927f4a93b13047318b9e4a3ff9d
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-24 13:27:35 -0500

    Completed task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ This module contains the BasicAuth class for Authentication """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,18 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split("Basic ", 1)[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """ Returns utf-8 decoded base64 header """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_base64 = b64decode(base64_authorization_header)
+            return decoded_base64.decode('utf-8')
+        except Exception:
+            return None
