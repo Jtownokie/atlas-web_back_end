92b76a9e4a1356ca6f8bfa6f92d51ca1c0787701
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-24 11:35:30 -0500

    Completed task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,14 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Requires Authorization """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        for ex_path in excluded_paths:
+            if path in ex_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Returns Auth Head """
