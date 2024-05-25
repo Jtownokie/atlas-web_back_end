81bfa3612fb96341bf587df39119bd0a4f11252d
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-25 11:44:47 -0500

    Completed task 9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ This module contains the BasicAuth class for Authentication """
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -33,3 +34,16 @@ class BasicAuth(Auth):
             return decoded_base64.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> Tuple[str, str]:
+        """ Returns User email and password """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+
+        return tuple(decoded_base64_authorization_header.split(":"))
