f06ad02f5c7935539c1188b98d8278a456f214b0
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 12:33:51 -0500

    Completed task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """ This module contains the SessionAuth class for Authentication """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ SessionAuth Subclass of Auth """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ This Method creates a session id for a user id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = uuid.uuid4()
+
+        self.user_id_by_session_id[f'{session_id}'] = user_id
+
+        return session_id
