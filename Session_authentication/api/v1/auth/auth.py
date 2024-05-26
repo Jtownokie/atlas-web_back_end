91a16da06fe0fc97fb0887231a2ac8dee02cddbe
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 13:43:23 -0500

    Moved method to Auth class

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """ This module contains the Auth class for Authentication """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth():
@@ -30,3 +31,10 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ Checks Current User """
         return None
+
+    def session_cookie(self, request=None):
+        """ This Method Returns a cookie value from a request """
+        if request is None:
+            return None
+
+        return request.cookies.get(f'{getenv("SESSION_NAME")}')
--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -27,10 +27,3 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
-
-    def session_cookie(self, request=None):
-        """ This Method Returns a cookie value from a request """
-        if request is None:
-            return None
-
-        return request.cookies.get(f'{getenv("SESSION_NAME")}')
