30d76d0fd25ce7d652b2cee4177e96e0a5e51fc9
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 12:22:06 -0500

    Completed task 1

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module contains the SessionAuth class for Authentication """
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAuth Subclass of Auth """
+    pass
