f4af66262401e3a036f635937c2b927f107d64cc
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 21:15:17 -0500

    Completed task 2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 from typing import TypeVar
 
@@ -35,3 +37,18 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Find a user by keyword argument filtering """
+        query = self._session.query(User)
+        for key, value in kwargs.items():
+            if key not in User.__table__.columns.keys():
+                raise InvalidRequestError()
+            query = query.filter(getattr(User, key) == value)
+
+        user = query.first()
+
+        if user is None:
+            raise NoResultFound()
+
+        return user
