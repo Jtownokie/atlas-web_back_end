102f19f4e7d4f062b3671ae06beef88b0c141b6f
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 23:13:45 -0500

    Completed task 10

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -35,6 +35,13 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """ Returns session ID """
+        user = self._db.find_user_by(email=email)
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
+
 
 def _hash_password(password: str) -> bytes:
     """ Returns a hashed, salted password """
