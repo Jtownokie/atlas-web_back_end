6eeb59718981b532e5bc7eb45544c3c9c5059af6
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 20:29:42 -0500

    Added documentation

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -29,6 +29,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """ Adds User to Database """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
