20ddf00ceb234d05e2cb682ddbd48d6965a7bb0c
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 23:59:50 -0500

    Completed task 16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -62,7 +62,7 @@ class Auth:
         """ Generates Reset Token """
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = _hash_password
+            reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except NoResultFound:
