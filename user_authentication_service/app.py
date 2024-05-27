e2b71caf937457318acb8fed9cd40b313ecf93d8
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 23:31:43 -0500

    Completed task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Route module for Basic Flask App
 """
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, make_response
 from auth import Auth
 
 
@@ -33,6 +33,26 @@ def users():
         return jsonify({"email": f"{user_email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST
+    Return:
+        - None
+    """
+    user_email = request.form.get("email")
+    user_pwd = request.form.get("password")
+
+    if AUTH.valid_login(user_email, user_pwd) is False:
+        abort(401)
+
+    session_id = AUTH.create_session(user_email)
+
+    response = make_response(jsonify({f"{user_email}": "logged in"}))
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
