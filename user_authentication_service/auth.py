3f2979d38fc5aa81fa1e03c16c75560dcf7363bb
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 23:58:52 -0500

    Completed task 16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -58,6 +58,16 @@ class Auth:
         self._db.update_user(user_id=user_id, session_id=None)
         return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generates Reset Token """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _hash_password
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """ Returns a hashed, salted password """
