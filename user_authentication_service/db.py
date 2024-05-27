303d9f8629c7571ebc826d1d72ce31f0f0f233ed
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 20:37:21 -0500

    Removed echo=true from engine

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -15,7 +15,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds User to Database """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
