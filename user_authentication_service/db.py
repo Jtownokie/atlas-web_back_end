1402f0be3c4f0bfa435fa5dadf43e7303cbc4d75
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-26 21:32:45 -0500

    Completed task 3

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -52,3 +52,13 @@ class DB:
             raise NoResultFound()
 
         return user
+
+    def update_user(self, user_id: str, **kwargs) -> None:
+        """ Updates User Columns """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if key not in User.__table__.columns.keys():
+                raise ValueError()
+            setattr(user, key, value)
+        self._session.commit()
+        return None
