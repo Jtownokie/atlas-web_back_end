d3f6fae89af9586e39bb60b2df73bbc32e1eb6a4
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-31 15:58:16 -0500

    Completed task 5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """ This module contains testing classes for client.py """
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 
 from client import GithubOrgClient
 from utils import get_json
@@ -23,3 +23,20 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_get_json.assert_called_once_with(expected_url)
         self.assertEqual(org_result, {'key': 'value'})
+
+    def test_public_repos_url(self):
+        """ Tests public_repos_url property """
+        expected_url = {
+            'repos_url': 'https://api.github.com/orgs/google/repos'
+        }
+
+        with patch.object(
+                GithubOrgClient,
+                'org',
+                PropertyMock(return_value={'repos_url': expected_url})):
+
+            client = GithubOrgClient('google')
+
+            result = client._public_repos_url
+
+            self.assertEqual(result, expected_url)
