2945ebfd6afe7f1e0cce39028b5823298795c20a
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-31 15:09:02 -0500

    Completed task 3

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """ This module contains the TestAccessNestedMap Class """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
 
 
@@ -45,3 +45,31 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
 
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ This class tests the memoize decorator """
+
+    def test_memoize(self):
+        class TestClass:
+            """ For testing memoize decorator"""
+            def a_method(self):
+                """ Test method """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ Property returned by memoize"""
+                return self.a_method()
+
+        with patch.object(
+                TestClass, 'a_method', return_value=42) as mock_method:
+            test_class = TestClass()
+
+            result1 = test_class.a_property
+            result2 = test_class.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            mock_method.assert_called_once()
