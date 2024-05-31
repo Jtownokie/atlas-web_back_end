62c66b325b4cc0938835c04c04c695b09c77680c
Jtownokie <christopher.stephens@atlasschool.com>
2024-05-31 15:34:09 -0500

    Completed task 4

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+""" This module contains testing classes for client.py """
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch, Mock
+
+from client import GithubOrgClient
+from utils import get_json
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ This class tests the GithubOrgClient class """
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json', return_value={'key': 'value'})
+    def test_org(self, org_name, mock_get_json):
+        """ Tests the org property """
+        client = GithubOrgClient(org_name)
+        org_result = client.org
+        expected_url = f"https://api.github.com/orgs/{org_name}"
+
+        mock_get_json.assert_called_once_with(expected_url)
+        self.assertEqual(org_result, {'key': 'value'})
