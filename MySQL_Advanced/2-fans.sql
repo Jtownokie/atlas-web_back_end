e86c0bedcfbe156bfcbf2245d7078d79d5de00bb
Jtownokie <christopher.stephens@atlasschool.com>
2024-06-12 21:01:38 +0000

    Corrected column names

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,11 +1,14 @@
 -- Creates a view and selects from to get desired output
 
+CREATE VIEW origin_fan_count AS
 SELECT
     origin,
-    SUM(nb_fans) AS nb_fans
+    SUM(fans) AS nb_fans
 FROM
     metal_bands
 GROUP BY
     origin
 ORDER BY
     nb_fans DESC;
+
+SELECT * FROM origin_fan_count;
---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,14 @@
+-- Script that lists all bands with specific style
+
+SELECT
+    band_name,
+    CASE
+        WHEN split IS NULL THEN YEAR(CURDATE()) - formed
+        ELSE split - formed
+    END AS lifespan
+FROM
+    metal_bands
+WHERE
+    style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
