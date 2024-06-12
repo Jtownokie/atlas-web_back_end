eb53a3cb0b27ca519bfaf8334a246031efa6c4da
Jtownokie <christopher.stephens@atlasschool.com>
2024-06-12 23:27:03 +0000

    Adjusting parameter names

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -3,17 +3,17 @@
 DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
+    IN proc_user_id INT
 )
 BEGIN
     DECLARE avg_score DECIMAL(10, 2);
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = proc_user_id;
 
     UPDATE users
     SET average_score = avg_score
-    WHERE id = user_id;
+    WHERE id = proc_user_id;
 END $$
 
 DELIMITER ;
