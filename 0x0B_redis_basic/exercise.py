0562454c06bd731340d7c112934ce2b7b51a5c01
Jtownokie <christopher.stephens@atlasschool.com>
2024-06-11 19:06:13 -0500

    Redefined count_calls key

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -9,7 +9,7 @@ from functools import wraps
 def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        key = f"{method.__qualname__}"
+        key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
