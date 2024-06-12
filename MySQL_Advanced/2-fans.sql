fa19565974d72be9a0c2bacfe2ba17f82dad4f19
Jtownokie <christopher.stephens@atlasschool.com>
2024-06-12 20:51:57 +0000

    Removed SELECT statement

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,5 @@
 -- Creates a view and selects from to get desired output
 
-CREATE VIEW origin_fan_count AS
 SELECT
     origin,
     SUM(nb_fans) AS nb_fans
@@ -10,5 +9,3 @@ GROUP BY
     origin
 ORDER BY
     nb_fans DESC;
-
-SELECT * FROM origin_fan_count;
