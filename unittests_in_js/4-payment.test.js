05602c11659eec98cb256baaa607655955857eb8
Jtownokie <christopher.stephens@atlasschool.com>
2024-06-26 13:08:59 -0500

    Completed task 4

---
+++ unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+// Task 3 Testing
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils')
+
+describe('sendPaymentRequestToApi', () => {
+  it('Stub function to reduce performance cost', () => {
+    calculateStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledWith(calculateStub, 'SUM', 100, 20);
+    sinon.assert.calledWith(consoleSpy, 'The total is: 10');
+
+    calculateStub.restore();
+    consoleSpy.restore();
+  });
+});
