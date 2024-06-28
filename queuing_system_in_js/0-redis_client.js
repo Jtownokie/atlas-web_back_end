2d4d1b24745ddb07c041f574c9bd2c2b31619f32
Jtownokie <christopher.stephens@atlasschool.com>
2024-06-27 20:16:46 -0500

    Completed task 2

--- queuing_system_in_js/0-redis_client.js
+++ queuing_system_in_js/0-redis_client.js
@@ -1,17 +1,13 @@
 // Task 1 Script
 import { createClient } from 'redis';
 
-async function initializeRedis() {
-  try {
-    const client = await createClient()
+const client = createClient();
 
-    client.on('error', (err) => {console.log('Redis client not connected to the server: ', err)});
-    client.on('connect', () => {console.log('Redis client connected to the server')});
-  } catch (err) {
-    console.error(err);
-  }
-  
-}
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
 
-initializeRedis();
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err);
+});
 
