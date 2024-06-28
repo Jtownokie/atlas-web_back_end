05ede12e7b65fe0b8fc08280212929c36c547735
Jtownokie <christopher.stephens@atlasschool.com>
2024-06-27 21:39:06 -0500

    Completed task 6

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+// Task 6 Script
+const kue = require('kue')
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'Howdy partner!'
+};
+
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.error('Notification job failed');
+});
